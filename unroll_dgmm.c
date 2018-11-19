e4e8b5ba6033fceb8f3d7f16f14a22c36ce2c79a
Albert C Cruz <acruz37@csub.edu>
2018-11-19 10:26:51 -0800

    Indentations

--- unroll_dgmm.c
+++ unroll_dgmm.c
@@ -59,8 +59,24 @@ void initRandMat( int n, double* A ) {
 // known as 'DGEMM' for double-precision general matrix multiply (GEMM). 
 // Assumes matrixes are square and of the same length ('n' is the length of 
 // one side).
-void dgemm( int n, double* A, double* B, double* C ) {   for ( int i = 0; i < n; i+=UNROLL*4 )      for ( int j = 0; j < n; j++ ) {         __m256d c[4];
-         for ( int x = 0; x < UNROLL; x++ )            c[x] = _mm256_loadu_pd(C+i+x*4+j*n);         for( int k = 0; k < n; k++ ) {            __m256d b = _mm256_broadcast_sd(B+k+j*n);
-            for (int x = 0; x < UNROLL; x++)               c[x] = _mm256_add_pd( c[x],                                     _mm256_mul_pd(_mm256_loadu_pd(A+n*k+x*4+i), b)
-                                   );
-         }         for ( int x = 0; x < UNROLL; x++ )            _mm256_storeu_pd(C+i+x*4+j*n, c[x]);      }}
+void dgemm( int n, double* A, double* B, double* C ) {
+    for ( int i = 0; i < n; i+=UNROLL*4 ) {
+        for ( int j = 0; j < n; j++ ) {	
+	    __m256d c[4];
+
+	    for ( int x = 0; x < UNROLL; x++ )
+                c[x] = _mm256_loadu_pd(C+i+x*4+j*n);
+
+            for( int k = 0; k < n; k++ ) {
+                __m256d b = _mm256_broadcast_sd(B+k+j*n);
+                for (int x = 0; x < UNROLL; x++)
+                    c[x] = _mm256_add_pd( c[x],
+                                          _mm256_mul_pd(_mm256_loadu_pd(A+n*k+x*4+i), b)
+                      			);
+            }
+        
+	    for ( int x = 0; x < UNROLL; x++ )
+	        _mm256_storeu_pd(C+i+x*4+j*n, c[x]);
+       	}
+    }
+}
